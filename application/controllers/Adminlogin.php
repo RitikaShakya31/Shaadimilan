a04d9a423cd7e32361369810c6e3bba78d3b7f5e
Ritika Shakya <ritikashakya31@gmail.com>
2025-01-11 16:01:15 +0530

    reinitlize

--- application/controllers/Adminlogin.php
+++ application/controllers/Adminlogin.php
@@ -4,16 +4,16 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Adminlogin extends CI_Controller
 {
 	public function __construct()
-    {
-        parent::__construct();
-        if (sessionId('user_id') != "") {
-            redirect(base_url("AdminDashboard"));
-        }
-    }
+	{
+		parent::__construct();
+		if (sessionId('user_id') != "") {
+			redirect(base_url("AdminDashboard"));
+		}
+	}
 	public function index()
 	{
 		$data['title'] = 'Admin login  ';
-		$data['projectTitle'] = 'Rishtey Marriage';
+		$data['projectTitle'] = 'Shadi Milan';
 		$this->load->view('admin/admin_login', $data);
 	}
 	public function validatelogin()
@@ -22,20 +22,15 @@ class Adminlogin extends CI_Controller
 			$email = $this->input->post('email');
 			$password = $this->input->post('password');
 			$user_id = $this->CommonModal->getRowById('admin_login', 'email', $email);
-
 			if ($user_id) {
 				if ($user_id[0]['password'] == $password) {
 					$this->session->set_userdata('user_id', $user_id[0]['id']);
 					$this->session->set_userdata('user_type', $user_id[0]['admin_type']);
 					redirect('AdminDashboard');
-				}
-				 else {
-
+				} else {
 					$this->session->set_userdata('msg', '<div class="alert alert-danger">Invalid Password</div>');
 				}
-			}
-			 else {
-
+			} else {
 				$this->session->set_userdata('msg', '<div class="alert alert-danger">Invalid username</div>');
 			}
 			redirect(base_url('admin'));
--- application/views/admin/admin_login.php
+++ application/views/admin/admin_login.php
@@ -1,59 +1,39 @@
-    <?php include('includes/header.php'); ?>
+<?php include('includes/header.php'); ?>
 
-<body>
-    <div class="account-pages my-5 pt-sm-5">
+<body style="background: url(assets/img/images/register-background2.png);">
+    <div class="account-page pt-sm-5" style="margin-top: 8%;">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-8 col-lg-6 col-xl-5">
-                    <div class="card overflow-hidden">
-                        <div class="bg-primary-subtle">
+                    <div class="card overflow-hidden"
+                        style="border-radius: 15px;box-shadow: rgb(147 147 147 / 35%) 0px 5px 15px;">
+                        <div style="background:#e4e4e4;border-bottom: 1px solid #80808042;padding: 15px 8px;">
                             <div class="row">
-                                <div class="col-7">
-                                    <div class="text-primary p-4">
-                                        <h5 class="text-primary">Welcome Back !</h5>
-                                        <!-- <p>Sign in to continue to Digital BCard.</p> -->
-                                        <?php
-                                        if ($this->session->has_userdata('msg')) {
-                                            echo $this->session->userdata('msg');
-                                            $this->session->unset_userdata('msg');
-                                        }
-                                        ?>
-                                    </div>
-                                </div>
-                                <div class="col-5 align-self-end">
-                                    <img src="<?= base_url() ?>assets/admin/images/profile-img.png" alt="" class="img-fluid" />
-                                </div>
+                                <?php
+                                if ($this->session->has_userdata('msg')) {
+                                    echo $this->session->userdata('msg');
+                                    $this->session->unset_userdata('msg');
+                                }
+                                ?>
+                                <img style="width: 200px;margin: auto;" src="<?= base_url() ?>assets/img/images/Shadi.png" alt=""/>
                             </div>
                         </div>
                         <div class="card-body pt-0">
-                            <!-- <div class="auth-logo">
-                                <a href="<?= base_url() ?>login" class="auth-logo-light">
-                                    <div class="avatar-md profile-user-wid mb-4">
-                                        <span class="avatar-title rounded-circle bg-light">
-                                            <img src="<?= base_url() ?>assets/admin/images/logo-light.svg" alt="" class="rounded-circle" height="34" />
-                                        </span>
-                                    </div>
-                                </a>
-
-                                <a href="<?= base_url() ?>login" class="auth-logo-dark">
-                                    <div class="avatar-md profile-user-wid mb-4">
-                                        <span class="avatar-title rounded-circle bg-light">
-                                            <img src="<?= base_url() ?>assets/image/digitallogo.png" alt="" class="rounded-circle" height="34" />
-                                        </span>
-                                    </div>
-                                </a>
-                            </div> -->
-                            <div class="p-2">
-                                <form class="form-horizontal" action="<?= base_url('adminlogin/validatelogin') ?>" method="post">
+                            <div class="p-3">
+                                <form class="form-horizontal" action="<?= base_url('adminlogin/validatelogin') ?>"
+                                    method="post">
                                     <div class="mb-3">
                                         <label for="username" class="form-label">Email</label>
-                                        <input type="email" class="form-control" id="admin_email" name="email" placeholder="Enter username" />
+                                        <input type="email" class="form-control" id="admin_email" name="email"
+                                            placeholder="Enter username" />
                                     </div>
 
                                     <div class="mb-3">
                                         <label class="form-label">Password</label>
                                         <div class="input-group auth-pass-inputgroup">
-                                            <input type="password" class="form-control" placeholder="Enter password" aria-label="Password" aria-describedby="password-addon" name="password" />
+                                            <input type="password" class="form-control" placeholder="Enter password"
+                                                aria-label="Password" aria-describedby="password-addon"
+                                                name="password" />
                                             <button class="btn btn-light" type="button" id="password-addon">
                                                 <i class="mdi mdi-eye-outline"></i>
                                             </button>
@@ -61,7 +41,8 @@
                                     </div>
 
                                     <div class="mt-3 d-grid">
-                                        <button class="btn btn-primary waves-effect waves-light" type="submit">
+                                        <button style="background: #b71855;border:none;"
+                                            class="btn btn-primary waves-effect waves-light" type="submit">
                                             Log In
                                         </button>
                                     </div>
